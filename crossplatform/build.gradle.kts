db21e7d669bce3388f7038f2704180b58ac56f1b
mbahgojol <profghozimahdi@gmail.com>
2023-08-25 11:28:53 +0700

    fixing ksp dependecy for all targets

--- crossplatform/build.gradle.kts
+++ crossplatform/build.gradle.kts
@@ -2,9 +2,6 @@
 
 import com.mbahgojol.convention.commonMain
 import com.mbahgojol.convention.commonTest
-import com.mbahgojol.convention.compileSdkVersion
-import com.mbahgojol.convention.libsExt
-import com.mbahgojol.convention.minSdkVersion
 
 plugins {
     id("mbahgojol.kotlin.multiplatform")
@@ -34,10 +31,6 @@ kotlin {
 
 android {
     namespace = "com.mbahgojol.crossplatform"
-    compileSdk = libsExt.compileSdkVersion
-    defaultConfig {
-        minSdk = libsExt.minSdkVersion
-    }
 }
 
 ksp {
